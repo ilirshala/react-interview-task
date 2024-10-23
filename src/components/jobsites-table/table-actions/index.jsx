2decdeedcf98002858c8f95983700232ad0dc287
ilirshala-nordra <ilir.shala111@gmail.com>
2024-10-23 12:37:20 +0200

    search input added width

--- src/components/jobsites-table/table-actions/index.jsx
+++ src/components/jobsites-table/table-actions/index.jsx
@@ -1,25 +1,21 @@
 import React from "react";
 import "./style.css";
 import { Flex, Input } from "antd";
-import {
-  InfoCircleTwoTone,
-  PlusOutlined,
-  SearchOutlined,
-} from "@ant-design/icons";
+import { PlusOutlined, SearchOutlined } from "@ant-design/icons";
 import ActionButton from "../../action-button";
 
 const TableActions = () => {
   return (
     <Flex className="tableActions" justify="space-between">
       <Flex gap={"small"} align="center">
-        <InfoCircleTwoTone twoToneColor={"#1890FF"} />
-        <p>Informative piece of text that can be used regarding this modal.</p>
+        <h4>Jobsites</h4>
       </Flex>
       <Flex gap={"small"} align="center">
         <Input
           size="medium"
           variant="filled"
-          placeholder="large size"
+          placeholder="Search a driver"
+          className="searchInput"
           prefix={<SearchOutlined />}
         />
         <ActionButton
